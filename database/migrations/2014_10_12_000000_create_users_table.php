ffd880acae6e945355950b1e9c1132c4d67c2a4f
james johnson <james212.dev@gmail.com>
2023-02-21 03:09:02 +1000

    register user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,28 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('user_no', 10)->unique();
+            $table->string('clinic_id', 6)->unique();
+            $table->string('clinic_name', 100);
+            $table->string('tel_no_new', 13);
+            $table->string('tel_num_new', 13);
+            $table->string('tel_no_old', 13)->nullable()->default(null);
+            $table->string('tel_num_old', 13)->nullable()->default(null);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('password', 30);
+            $table->date('password_expired_at');
+            $table->dateTime('login_at')->nullable();
+            $table->date('register_at')->default(new DateTime());
+            $table->date('license_at')->nullable();
+            $table->boolean('patient_reg_opt')->default(false);
+            $table->boolean('reception_opt')->default(false);
+            $table->boolean('reserve_opt')->default(false);
+            $table->boolean('opt1')->default(false);
+            $table->boolean('opt2')->default(false);
+            $table->tinyInteger('db_no')->default(0);
+            $table->boolean('maintainance_lock')->default(false);
+            $table->string('memo')->nullable();
+            $table->tinyInteger('cust_status')->default(1);
             $table->timestamps();
         });
     }
