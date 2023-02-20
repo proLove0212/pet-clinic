1e4328458b6476e7d366f82ba40471fb292df75d
james johnson <james212.dev@gmail.com>
2023-02-21 03:27:41 +1000

    register customer table

---
+++ database/migrations/2023_02_20_171042_create_customer_table.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('db_no')->default(0);
+            $table->string('clinic_id', 10)->unique();
+            $table->string('cust_no', 10)->unique();
+            $table->string('family_name', 100)->default('0');
+            $table->string('name', 100)->default('0');
+            $table->string('family_name_hiragana', 100)->default('0');
+            $table->string('name_hiragana', 100)->default('0');
+            $table->string('address', 100)->nullable()->default('0');
+            $table->string('tel1', 100)->nullable()->default('null');
+            $table->string('tel2', 100)->nullable()->default('null');
+            $table->string('tel3', 100)->nullable()->default('null');
+            $table->string('tel4', 100)->nullable()->default('null');
+            $table->string('tel5', 100)->nullable()->default('null');
+            $table->string('tel6', 100)->nullable()->default('null');
+            $table->string('tel7', 100)->nullable()->default('null');
+            $table->string('tel8', 100)->nullable()->default('null');
+            $table->string('tel1_num', 100)->nullable()->default('null');
+            $table->string('tel2_num', 100)->nullable()->default('null');
+            $table->string('tel3_num', 100)->nullable()->default('null');
+            $table->string('tel4_num', 100)->nullable()->default('null');
+            $table->string('tel5_num', 100)->nullable()->default('null');
+            $table->string('tel6_num', 100)->nullable()->default('null');
+            $table->string('tel7_num', 100)->nullable()->default('null');
+            $table->string('tel8_num', 100)->nullable()->default('null');
+            $table->string('tel1_last4', 100)->nullable()->default('null');
+            $table->string('tel2_last4', 100)->nullable()->default('null');
+            $table->string('tel3_last4', 100)->nullable()->default('null');
+            $table->string('tel4_last4', 100)->nullable()->default('null');
+            $table->string('tel5_last4', 100)->nullable()->default('null');
+            $table->string('tel6_last4', 100)->nullable()->default('null');
+            $table->string('tel7_last4', 100)->nullable()->default('null');
+            $table->string('tel8_last4', 100)->nullable()->default('null');
+            $table->string('email')->unique();
+            $table->string('classification', 10)->nullable()->default('null');
+            $table->date('last_coming_at')->nullable()->default(new DateTime());
+            $table->date('next_coming_at')->nullable()->default(new DateTime());
+            $table->string('next_reason', 100)->nullable()->default('text');
+            $table->boolean('cust_valid')->nullable()->default(true);
+            $table->boolean('replace')->nullable()->default(false);
+            $table->string('edit_id', 100)->nullable()->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
