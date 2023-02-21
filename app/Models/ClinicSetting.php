d9a726e2b792dea4d6912d287ae49fdc01310aff
james johnson <james212.dev@gmail.com>
2023-02-21 21:35:37 +1000

    add ClinicSetting model

---
+++ app/Models/ClinicSetting.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClinicSetting extends Model
+{
+    use HasFactory;
+
+    protected $table = 'clinic_settings';
+
+    protected $fillable = [
+        'user_id',
+        'visit_reason',
+        'visit_disp_order',
+        'take_time',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+
+    ];
+}
