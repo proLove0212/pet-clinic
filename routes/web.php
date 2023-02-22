4f1dc7884e8bb3fa0124567c3a951aea3e12dede
james johnson <james212.dev@gmail.com>
2023-02-22 16:49:48 +1000

    add template and layout of main and auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/login', function () {
+    return view('temp');
+});
+
+
+Route::get('/home', function () {
+    return view('temp');
+});
