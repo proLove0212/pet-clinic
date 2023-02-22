f4f83c9fa76d02639da40c1effe5d70b969f807b
james johnson <james212.dev@gmail.com>
2023-02-22 18:37:26 +1000

    add authcontroller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+
+    public function admin_login(Request $req){
+
+    }
+
+    public function user_login(Request $req){
+
+    }
+
+    public function customer_login(Request $req){
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/login', function () {
+Route::get('/user/login', function () {
     return view('temp');
 });
 
-
-Route::get('/home', function () {
+Route::get('/customer/login', function () {
     return view('temp');
 });
+
+Route::get('/admin/login', function () {
+    return view('temp');
+});
+
+Route::post('/admin/login', 'AuthController@admin_login')->name('admin_login');
+Route::post('/user/login', 'AuthController@user_login')->name('user_login');
+Route::post('/customer/login', 'AuthController@customer_login')->name('customer_login');
+
+// Route::middleware([EnsureTokenIsValid::class])->group(function () {
+
+// });
+
