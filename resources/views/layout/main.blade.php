ace6fdcf485ec2a6594f2019015cb5e052584303
james johnson <james212.dev@gmail.com>
2023-03-06 21:19:04 +0900

    final

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,7 +4,7 @@
     <head>
 
         <meta charset="utf-8" />
-        <title>PetClinic | @yield('title')</title>
+        <title>Web顧客情報検索サービス | @yield('title')</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="Themesbrand" name="author" />
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,91 +1,200 @@
 <!DOCTYPE html>
-<html lang="en" class="no-js">
+<html>
+
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PetClinic</title>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <!-- App favicon -->
+  <link rel="shortcut icon" href="{{url('assets/images/favicon.ico')}}">
 
-    <!-- App favicon -->
-    <link rel="shortcut icon" href="{{url('assets/images/favicon.ico')}}">
-    <link rel="stylesheet" href="{{url('assets/css/custom.css')}}">
+  <title> Web顧客情報検索サービス </title>
 
-    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,600" rel="stylesheet">
-    <link rel="stylesheet" href="{{url('landing/css/style.css')}}">
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="{{url('landing/css/bootstrap.css')}}" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+
+  <!--owl slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+  <!-- Custom styles for this template -->
+  <link href="{{url('landing/css/style.css')}}" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="{{url('landing/css/responsive.css')}}" rel="stylesheet" />
+
 </head>
-<body class="is-boxed has-animations">
-    <div class="body-wrap">
-        <header class="site-header">
-            <div class="container">
-                <div class="site-header-inner">
-                    <div class="brand header-brand">
-                        <h1 class="m-0">
-							<a href="#">
-							</a>
-                        </h1>
-                    </div>
-                </div>
-            </div>
-        </header>
 
-        <main>
-            <section class="hero">
-                <div class="container">
-                    <div class="main-container">
-                        <div class="section-logo">
-                            <img src="https://preview.colorlib.com/theme/anipat/img/banner/dog.png.webp" alt="">
-                        </div>
-                        <div class="section_adv">
-	                        <h1 class="hero-title mt-0">ペットクルーカルテ</h1>
-	                        <p class="hero-paragraph">夜間や病院の休診日など、病院への電話を携帯電話に転送して応対している病院様向けに、外出先でも簡単な顧客情報が検索・確認できるWeb顧客情報検索サービスです。<br>
-                                これにより、外出先においてもWeb接続が可能な携帯端末(iPhoneやAndroidなどのスマートフォン)やタブレットPC、ノートPCなどがあればどこでもアクセス可能です。</p>
-	                        <div class="hero-cta">
-                                <a class="button button-primary" href="{{url('user/login')}}">ユーザーログイン</a>
-                                <a class="button" id="customer_login">顧客ログイン</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </main>
+<body>
 
-        <footer class="site-footer">
-            <div class="container">
-                <div class="site-footer-inner">
-                    <div class="brand footer-brand">
+  <div class="hero_area">
 
-                    </div>
-                    <ul class="footer-social-links list-reset">
-                        <li>
-                            <a href="#">
-                                <span class="screen-reader-text">Facebook</span>
-                                <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M6.023 16L6 9H3V6h3V4c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V6H13l-1 3H9.28v7H6.023z" fill="#0270D7"/>
-                                </svg>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="screen-reader-text">Twitter</span>
-                                <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M16 3c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4C.7 7.7 1.8 9 3.3 9.3c-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H0c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4C15 4.3 15.6 3.7 16 3z" fill="#0270D7"/>
-                                </svg>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="screen-reader-text">Google</span>
-                                <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M7.9 7v2.4H12c-.2 1-1.2 3-4 3-2.4 0-4.3-2-4.3-4.4 0-2.4 2-4.4 4.3-4.4 1.4 0 2.3.6 2.8 1.1l1.9-1.8C11.5 1.7 9.9 1 8 1 4.1 1 1 4.1 1 8s3.1 7 7 7c4 0 6.7-2.8 6.7-6.8 0-.5 0-.8-.1-1.2H7.9z" fill="#0270D7"/>
-                                </svg>
-                            </a>
-                        </li>
-                    </ul>
-                    <div class="footer-copyright">&copy; 2023 PetClinic, all rights reserved</div>
-                </div>
-            </div>
-        </footer>
+    <div class="hero_bg_box">
+      <img src="{{url('landing/images/hero-bg.png')}}" alt="">
     </div>
 
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="">
+            <span>
+                ペットクルーカルテ
+            </span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="{{url('/user/login')}}">ユーザーログイン</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="">顧客ログイン</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+    <!-- slider section -->
+    <section class="slider_section ">
+      <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-7">
+                  <div class="detail-box">
+                    <h1>
+                        Web顧客情報検索サービス
+                    </h1>
+                    <p style="line-height:2; font-size:20px">
+                        夜間や病院の休診日など、病院への電話を携帯電話に転送して応対している病院様向けに、外出先でも簡単な顧客情報が検索・確認できるWeb顧客情報検索サービスです。
+
+                        これにより、外出先においてもWeb接続が可能な携帯端末(iPhoneやAndroidなどのスマートフォン)やタブレットPC、ノートPCなどがあればどこでもアクセス可能です。
+
+                        利用できる端末：
+                        携帯電話（iPhoneやAndroidなどのスマートホン）、タブレット、ノートPCなど、インターネットに接続できる端末から利用可能
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item ">
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-7">
+                  <div class="detail-box">
+                    <h1>
+                        SSLによる通信
+                    </h1>
+                    <p style="line-height:2; font-size:20px">
+                        このサービス内の通信は全てSSLという通信方式によって行われています。
+
+                        SSLとはインターネット上で情報を暗号化して送受信できる仕組みで、ネットワーク上に流れる通信を暗号化しているため、個人情報などの大切なデータを安全にやりとりできます。
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-7">
+                  <div class="detail-box">
+                    <h1>
+                        デモで操作の確認をしたい
+                    </h1>
+                    <p style="line-height:2; font-size:20px">
+                        まずは、実際の使い勝手を知るためにデモログイン用のIDとPWも用意してあります。
+                        メール本文に病院名と住所・電話番号明記の上ご連絡ください。
+                        お問い合わせ先メール：　
+                    </p>
+                    <div class="">
+                        <a href="mailto:support@petcrew.jp?subject=Ｗｅｂ顧客検索のデモが見たい">Web顧客検索デモが見たい</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-7">
+                  <div class="detail-box">
+                    <h1>
+                        サービスの利用をご希望の場合
+                    </h1>
+                    <p style="line-height:2; font-size:20px">
+                        実際にサービスのご利用を希望されるユーザー様は、以下のメールにご連絡ください。　専用のIDとパスワードを発行いたします。　
+                        メール本文に病院名と住所・電話番号明記の上ご連絡ください。
+                        お問い合わせ先メール：　
+                    </p>
+                    <div class="">
+                        <a href="mailto:order@petcrew.jp?subject=Web顧客検索のサービスを利用したい">Web顧客検索のサービスを利用したい</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <ol class="carousel-indicators">
+          <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
+          <li data-target="#customCarousel1" data-slide-to="1"></li>
+          <li data-target="#customCarousel1" data-slide-to="2"></li>
+          <li data-target="#customCarousel1" data-slide-to="3"></li>
+        </ol>
+      </div>
+
+    </section>
+    <!-- end slider section -->
+  </div>
+
+
+  <!-- footer section -->
+  <footer class="footer_section">
+    <div class="container">
+      <div class="footer-info">
+        <p>
+          &copy; <span id="displayYear"></span> All Rights Reserved</span>
+
+        </p>
+
+      </div>
+    </div>
+  </footer>
+  <!-- footer section -->
+
+  <!-- jQery -->
+  <script type="text/javascript" src="{{url('landing/js/jquery-3.4.1.min.js')}}"></script>
+  <!-- popper js -->
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <!-- bootstrap js -->
+  <script type="text/javascript" src="{{url('landing/js/bootstrap.js')}}"></script>
+  <!-- owl slider -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+  <!-- custom js -->
+  <script type="text/javascript" src="{{url('landing/js/custom.js')}}"></script>
+  <!-- Google Map -->
+
 </body>
+
 </html>
